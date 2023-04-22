b912de6799e03ded960f65b09d03c704848a4f39
mizza-cz <mizza.cz666@gmail.com>
2023-04-22 20:12:15 +0200

    add modal id

--- src/components/forum-section/forum-section.js
+++ src/components/forum-section/forum-section.js
@@ -1,9 +1,11 @@
 $("#myModal").on("show.bs.modal", function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
   var recipient = button.data("whatever");
+  var modalID = button.data("id");
+
   var modal = $(this);
   modal.find(".modal-title").text("Přidat příspěvek ");
-  //   modal.find(".modal-body input").val(recipient);
+  modal.find("#index").val(modalID);
   if (modal.find(".c-forum__reaction").length > 0) {
     modal.find(".c-forum__reaction").text("Reakce na příspěvek:  " + recipient);
   }
