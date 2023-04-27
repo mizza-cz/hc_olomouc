0194346e29e51f2d29b0437f212f10742a49c374
mizza-cz <mizza.cz666@gmail.com>
2023-04-27 09:22:47 +0200

    add fixes

---
+++ src/components/hp-board-section/hp-board-section.js
@@ -0,0 +1,15 @@
+$(".hp-board__social-wrap .hp-board__social-tab").on("click", function (event) {
+  var id = $(this).attr("data-id");
+  $(".hp-board__social-wrap")
+    .find(".hp-board__social")
+    .removeClass("tab-active")
+    .hide();
+  $(".hp-board__social-wrap .hp-board__social-tabs")
+    .find(".hp-board__social-tab")
+    .removeClass("active");
+  $(this).addClass("active");
+  $("#" + id)
+    .addClass("tab-active")
+    .fadeIn();
+  return false;
+});
--- src/components/topstory-section/topstory-section.js
+++ src/components/topstory-section/topstory-section.js
@@ -11,3 +11,4 @@ $(document).ready(function () {
       $(".topstory__content").data("topstory_current", idTopstory);
   });
 });
+
