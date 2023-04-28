0402337fe2969027ad8ee0b5d8d3a0b9781f9f4d
mizza-cz <mizza.cz666@gmail.com>
2023-04-28 13:13:42 +0200

    add slider mladez

--- src/components/hp-tabs-section/hp-tabs-section.js
+++ src/components/hp-tabs-section/hp-tabs-section.js
@@ -6,3 +6,19 @@ $(function () {
     $(this).tab("show");
   });
 });
+$(".hp-tabs__matches").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow:
+    '<button class="c-matchboard__btn c-matchboard__btnprev"><img src="images/ico/slider-left.svg" loading="lazy" alt="" /></button> ',
+  nextArrow:
+    ' <button class="c-matchboard__btn  c-matchboard__btnnext"><img src="images/ico/slider-right.svg" loading="lazy" alt="" /></button>',
+  responsive: [
+    {
+      breakpoint: 1238,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
